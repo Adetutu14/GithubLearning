ff7b46553b2bafe6aac9f4b1fd011c6980184a26
Adetutu14 <adetutuadejuyigbe@gmail.com>
2024-05-23 20:24:55 +0100

    Added a new python file

---
+++ log_file_extract.py
@@ -0,0 +1,9 @@
+import os
+from datetime import datetime
+#import re
+import pandas as pd
+from sqlalchemy import create_engine
+import ast # Abstract syntax tree used for obtaining actual python objects from string
+from dotenv import dotenv_values
+dotenv_values()
+
